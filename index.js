2bb232175c6ab566b31423ac8bc9fd790696e6ba
Shahar Kedar <shahar@gmail.com>
2020-03-26 23:07:33 +0200

    First version of Q
    The first command is 'publish' which allows sending messages to Kafka
    from the command line

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const program = require('caporal');
+const { version } = require('./package.json');
+const fs = require('fs');
+
+async function main() {
+  program
+    .version(version);
+
+  const commandsDirectory = fs.opendirSync('./lib/commands');
+  for await (const commandFile of commandsDirectory) {
+    const command = require(`./lib/commands/${commandFile.name}`);
+    command(program);
+  }
+
+  await program.parse(process.argv);
+}
+
+main().then(() => {
+  process.exit(0);
+}).catch((error) => {
+  console.error(error)
+  process.exit(-1);
+});
